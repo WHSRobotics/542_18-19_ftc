14fe003f273e373e2712f55f061293a6a9193a8d
Amar <amargandhi10@gmail.com>
2018-09-17 14:26:55 -0700

    FIRST Commit

---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/tests/OmniArmTest.java
@@ -0,0 +1,15 @@
+package org.whitneyrobotics.ftc.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class OmniArmTest extends OpMode{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
