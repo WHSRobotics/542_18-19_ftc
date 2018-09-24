c8de90e4068e81e993b2a0f2422b88b7a581257c
Varun Bharadwaj <epicvarun@outlook.com>
2018-09-24 16:09:25 -0700

    Tried adding tensorflow.
    Followed steps 1, 2, and 3 in the YoloParser readme, but didn't know where to go from there. https://github.com/kerrj/yoloparser/blob/master/README.md#using-a-pretrained-neural-net-in-your-android-app

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/autoop/WHSAuto.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/autoop/WHSAuto.java
@@ -2,6 +2,8 @@ package org.whitneyrobotics.ftc.autoop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.whitneyrobotics.ftc.tests.RotateToTargetTest;
+
 public class WHSAuto extends OpMode{
     @Override
     public void init() {
